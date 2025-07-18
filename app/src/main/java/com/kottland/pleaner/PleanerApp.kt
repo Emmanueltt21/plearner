808259f15773a82be7e7657b5e2ca4b6a0e5a16f
Emmanuel Taah <81640127+Emmanueltt21@users.noreply.github.com>
2025-07-18 23:26:57 +0100

    projet phase 1 done

--- app/src/main/java/com/kottland/pleaner/PleanerApp.kt
+++ app/src/main/java/com/kottland/pleaner/PleanerApp.kt
@@ -3,5 +3,11 @@ package com.kottland.pleaner
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+
 @HiltAndroidApp
-class PleanerApp : Application()
+class PleanerApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialization code can go here if needed
+    }
+}
